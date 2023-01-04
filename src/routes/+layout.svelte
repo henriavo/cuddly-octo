6ae73a06b2ee8dc392c08a9dc4b14fbd78de2f6e
henriavo <3028339+henriavo@users.noreply.github.com>
2023-01-03 23:44:43 -0600

    refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,7 @@
     vertical-align: -.125em;
     fill: currentColor;
     }
-    </style>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+  </style>
 
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
     <symbol id="bootstrap" viewBox="0 0 118 94">
