a98a404192e23445d52e28f4926ccf88715c8c3b
Henri Idrovo <3028339+henriavo@users.noreply.github.com>
2023-09-18 23:01:44 -0500

    s3 bucket no longer public, so now fetching photos with presigned urls.

--- src/routes/photos/+page.server.js
+++ src/routes/photos/+page.server.js
@@ -1,22 +1,62 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
+
 import { USER_PASS } from '$env/static/private';
 import { USER_NAME } from '$env/static/private';
 import { MONGO_SERVER } from '$env/static/private';
+
+import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
+import { fromIni } from '@aws-sdk/credential-providers';
+import { formatUrl } from '@aws-sdk/util-format-url';
+import { HttpRequest } from '@smithy/protocol-http';
+import { parseUrl } from '@smithy/url-parser';
+import { Hash } from '@smithy/hash-node';
 
 const url = 'mongodb+srv://<<user>>:<<credential>>@<<server>>/?retryWrites=true&w=majority';
 const fullUrl = url
 	.replace('<<user>>', USER_NAME)
 	.replace('<<credential>>', USER_PASS)
 	.replace('<<server>>', MONGO_SERVER);
+
 const client = new MongoClient(fullUrl, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1
 });
+
 // const client = new MongoClient('mongodb://localhost:27017');
 const dbName = 'cuddly_octo';
 
-async function run() {
+const createPresignedUrlWithoutClient = async ({ region, bucket, key }) => {
+	const url = parseUrl(`https://${bucket}.s3.${region}.amazonaws.com/${key}`);
+	const presigner = new S3RequestPresigner({
+		credentials: fromIni(),
+		region,
+		sha256: Hash.bind(null, 'sha256')
+	});
+
+	const signedUrlObject = await presigner.presign(new HttpRequest(url));
+	return formatUrl(signedUrlObject);
+};
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	const arrayresult = loadLikes();
+	const map = {};
+	map['IMG_1400.JPG'] = await loadPictures('IMG_1400.JPG');
+	map['IMG_1401.JPG'] = await loadPictures('IMG_1401.JPG');
+	map['IMG_1402.JPG'] = await loadPictures('IMG_1402.JPG');
+	map['IMG_1403.JPG'] = await loadPictures('IMG_1403.JPG');
+	map['IMG_1404.JPG'] = await loadPictures('IMG_1404.JPG');
+	map['IMG_1405.JPG'] = await loadPictures('IMG_1405.JPG');
+	map['IMG_1406.JPG'] = await loadPictures('IMG_1406.JPG');
+	map['IMG_1407.JPG'] = await loadPictures('IMG_1407.JPG');
+	map['IMG_1408.JPG'] = await loadPictures('IMG_1408.JPG');
+	map['IMG_1409.JPG'] = await loadPictures('IMG_1409.JPG');
+
+	return { likesArray: arrayresult, photoUrls: map };
+}
+
+async function loadLikes() {
 	try {
 		await client.connect();
 		console.log('connected successfully to mongo db.');
@@ -24,9 +64,9 @@ async function run() {
 		const collection = db.collection('picture_likes');
 		const cursor = collection.find().sort({ _id: 1 });
 		const rrr = await cursor.toArray();
-		console.log('successful load from db. all records below:');
+		console.log('successful load from db.');
 		rrr.forEach(function (item, index) {
-			console.log('\t' + index + ' ' + item._id + ' ' + item.count + ' ' + item.upload_date);
+			//console.log('\t' + index + ' ' + item._id + ' ' + item.count + ' ' + item.upload_date);
 			item.elapsed_days = getDaysElapsed(item.upload_date);
 		});
 		return rrr;
@@ -37,10 +77,26 @@ async function run() {
 	}
 }
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-	const arrayresult = run();
-	return { likesArray: arrayresult };
+async function loadPictures(name) {
+	const REGION = 'us-east-2';
+	const BUCKET = 'henri-public-bucket';
+	const KEY = name;
+
+	try {
+		const noClientUrl = await createPresignedUrlWithoutClient({
+			region: REGION,
+			bucket: BUCKET,
+			key: KEY
+		});
+
+		console.log('Presigned URL created!');
+		// console.log(noClientUrl);
+		// console.log('\n');
+
+		return noClientUrl;
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 function getDaysElapsed(dateStr1) {
--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -13,70 +13,70 @@
 				<Photo
 					photoId="1409"
 					likeCount={data.likesArray[9].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1409.JPG"
+					imageUrl={data.photoUrls['IMG_1409.JPG']}
 					caption="We were having lunch at the morton arboretum. She was looking for her lunch too 👀"
 					timestamp={data.likesArray[9].elapsed_days}
 				/>
 				<Photo
 					photoId="1408"
 					likeCount={data.likesArray[8].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1408.JPG"
+					imageUrl={data.photoUrls['IMG_1408.JPG']}
 					caption="This fluffy hoodie witht the ears is my favorite"
 					timestamp={data.likesArray[8].elapsed_days}
 				/>
 				<Photo
 					photoId="1407"
 					likeCount={data.likesArray[7].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1407.JPG"
+					imageUrl={data.photoUrls['IMG_1407.JPG']}
 					caption="Messy hair baby, she just woke up from a nap"
 					timestamp={data.likesArray[7].elapsed_days}
 				/>
 				<Photo
 					photoId="1406"
 					likeCount={data.likesArray[6].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1406.JPG"
+					imageUrl={data.photoUrls['IMG_1406.JPG']}
 					caption="This lamb onesie is cute"
 					timestamp={data.likesArray[6].elapsed_days}
 				/>
 				<Photo
 					photoId="1405"
 					likeCount={data.likesArray[5].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1405.JPG"
+					imageUrl={data.photoUrls['IMG_1405.JPG']}
 					caption="Big smile after a meal"
 					timestamp={data.likesArray[5].elapsed_days}
 				/>
 				<Photo
 					photoId="1400"
 					likeCount={data.likesArray[0].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1400.JPG"
+					imageUrl={data.photoUrls['IMG_1400.JPG']}
 					caption="Good morning baby"
 					timestamp={data.likesArray[0].elapsed_days}
 				/>
 				<Photo
 					photoId="1401"
 					likeCount={data.likesArray[1].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1401.JPG"
+					imageUrl={data.photoUrls['IMG_1401.JPG']}
 					caption="Mid day nap"
 					timestamp={data.likesArray[1].elapsed_days}
 				/>
 				<Photo
 					photoId="1403"
 					likeCount={data.likesArray[3].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1403.JPG"
+					imageUrl={data.photoUrls['IMG_1403.JPG']}
 					caption="With her best buddy Benny"
 					timestamp={data.likesArray[3].elapsed_days}
 				/>
 				<Photo
 					photoId="1404"
 					likeCount={data.likesArray[4].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1404.JPG"
+					imageUrl={data.photoUrls['IMG_1404.JPG']}
 					caption="A good nap after a meal"
 					timestamp={data.likesArray[4].elapsed_days}
 				/>
 				<Photo
 					photoId="1402"
 					likeCount={data.likesArray[2].count}
-					imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1402.JPG"
+					imageUrl={data.photoUrls['IMG_1402.JPG']}
 					caption="I'm getting good at this swadle thing"
 					timestamp={data.likesArray[2].elapsed_days}
 				/>
@@ -88,41 +88,41 @@
 
 <Modal
 	modalId="1400"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1400.JPG"
+	imageUrl={data.photoUrls['IMG_1400.JPG']}
 />
 <Modal
 	modalId="1401"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1401.JPG"
+	imageUrl={data.photoUrls['IMG_1401.JPG']}
 />
 <Modal
 	modalId="1402"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1402.JPG"
+	imageUrl={data.photoUrls['IMG_1402.JPG']}
 />
 <Modal
 	modalId="1403"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1403.JPG"
+	imageUrl={data.photoUrls['IMG_1403.JPG']}
 />
 <Modal
 	modalId="1404"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1404.JPG"
+	imageUrl={data.photoUrls['IMG_1404.JPG']}
 />
 <Modal
 	modalId="1405"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1405.JPG"
+	imageUrl={data.photoUrls['IMG_1405.JPG']}
 />
 <Modal
 	modalId="1406"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1406.JPG"
+	imageUrl={data.photoUrls['IMG_1406.JPG']}
 />
 <Modal
 	modalId="1407"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1407.JPG"
+	imageUrl={data.photoUrls['IMG_1407.JPG']}
 />
 <Modal
 	modalId="1408"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1408.JPG"
+	imageUrl={data.photoUrls['IMG_1408.JPG']}
 />
 <Modal
 	modalId="1409"
-	imageUrl="https://henri-public-bucket.s3.us-east-2.amazonaws.com/IMG_1409.JPG"
+	imageUrl={data.photoUrls['IMG_1409.JPG']}
 />
