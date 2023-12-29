0c93afd517e9977f0f4a5275fcd4f272ba46c0fa
Henri Idrovo <git@holahenri.mozmail.com>
2023-12-29 14:48:23 -0600

    upload picture with sequential filename success

--- src/routes/upload/+page.server.js
+++ src/routes/upload/+page.server.js
@@ -1,3 +1,9 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+import { USER_PASS } from '$env/static/private';
+import { USER_NAME } from '$env/static/private';
+import { MONGO_SERVER } from '$env/static/private';
+
 import https from 'https';
 import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
 import { fromIni, fromEnv } from '@aws-sdk/credential-providers';
@@ -5,6 +11,21 @@ import { formatUrl } from '@aws-sdk/util-format-url';
 import { HttpRequest } from '@smithy/protocol-http';
 import { parseUrl } from '@smithy/url-parser';
 import { Hash } from '@smithy/hash-node';
+import { get } from 'http';
+
+const url = 'mongodb+srv://<<user>>:<<credential>>@<<server>>/?retryWrites=true&w=majority';
+const fullUrl = url
+	.replace('<<user>>', USER_NAME)
+	.replace('<<credential>>', USER_PASS)
+	.replace('<<server>>', MONGO_SERVER);
+
+const client = new MongoClient(fullUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1
+});
+
+const dbName = 'cuddly_octo';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
@@ -16,19 +37,37 @@ export const actions = {
 			console.log('No file was uploaded!');
 		} else {
 			console.log('File uploaded!');
-			// query db for next file name available. i.e. photo001.jpg or photo002.jpg
-			// pass the file name to the uploadPicture function
-			console.log(file);
-			uploadPicture(file);
+			let fileNumber = await getFileName();
+			console.log('$$$$: ' + fileNumber);
+			//console.log(file);
+			uploadPicture(file, fileNumber);
 		}
 	}
 };
 
 // ****************************************************************************************************
-async function uploadPicture(file) {
+
+async function getFileName() {
+	try {
+		await client.connect();
+		console.log('connected successfully to mongo db.');
+		const db = client.db(dbName);
+		const collection = db.collection('pictures');
+		const cursor = collection.find().sort({ _id: 1 });
+		const rrr = await cursor.toArray();
+		console.log('successful load from db.');
+		return parseInt(rrr.pop()._id) + 1;
+	} catch (error) {
+		console.error(`ERRORRR: ${error}`);
+	} finally {
+		await client.close();
+	}
+}
+
+async function uploadPicture(file, fileNumber) {
 	const REGION = 'us-east-2';
 	const BUCKET = 'henri-public-bucket';
-	const KEY = 'assasins005.jpg';
+	const KEY = 'IMG_' + fileNumber + '.JPG';
 
 	try {
 		const noClientUrl = await createPresignedUrlWithoutClientPut({
@@ -99,6 +138,3 @@ async function put(url, file) {
 		}
 	});
 }
-
-
-
