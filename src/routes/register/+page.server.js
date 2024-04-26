79e4daf2eaecbf0ef33a1fac996fbe33942bfa85
Henri Idrovo <3028339+henriavo@users.noreply.github.com>
2024-04-26 10:33:26 -0500

    check for existing email

--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -1,3 +1,25 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+import { USER_PASS } from '$env/static/private';
+import { USER_NAME } from '$env/static/private';
+import { MONGO_SERVER } from '$env/static/private';
+
+const url = 'mongodb+srv://<<user>>:<<credential>>@<<server>>/?retryWrites=true&w=majority';
+const fullUrl = url
+	.replace('<<user>>', USER_NAME)
+	.replace('<<credential>>', USER_PASS)
+	.replace('<<server>>', MONGO_SERVER);
+
+const client = new MongoClient(fullUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1
+});
+
+// const client = new MongoClient('mongodb://localhost:27017');
+const DB_NAME = 'cuddly_octo';
+const DB_COLLECTION = 'users';
+
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ request }) => {
@@ -8,9 +30,32 @@ export const actions = {
 		console.log(password);
 		const password_confirm = data.get('password');
 		console.log(password_confirm);
+		await doesUserAlreadyExist(email);
 	}
 };
 
+async function doesUserAlreadyExist(email) {
+	try {
+		await client.connect();
+		console.log('connected successfully to mongo db.');
+		const db = client.db(DB_NAME);
+		const collection = db.collection(DB_COLLECTION);
+		const cursor = collection.find({ email: email });
+		const count = await cursor.count();
+		if (count !== 0) {
+			console.log('User already exists');
+			return true;
+		} else {
+			console.log('User does not exist');
+			return false;
+		}
+	} catch (error) {
+		console.error(`ERRORRR: ${error}`);
+	} finally {
+		await client.close();
+	}
+}
+
 // connect to Mongo DB
-// query and check for duplicate email. SELECT * FROM USERS WHERE email = email. if count!=0, return error message. 
+// query and check for duplicate email. SELECT * FROM USERS WHERE email = email. if count!=0, return error message.
 // if no duplicate, create new user. INSERT INTO USERS (email, password) VALUES (email, password).
